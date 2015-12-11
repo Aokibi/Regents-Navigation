4df93209888d6e4b4bd462a1077c7f428928d651
Kayla Bonnstetter <bonnstet@stolaf.edu>
2015-12-11 16:12:23 -0600

    Updates to package.json so that hopefully the floor-map shows up.

--- source/components/floor-map.js
+++ source/components/floor-map.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import ImageLoader from 'react-imageloader'
 
 /* Displays the 2D floor map. 
    Currently not implemented */
 class FloorMap extends React.Component {
 	render() {
 		return(
-			<ImageLoader
-				src='/images/map_floor_2.png'
-				wrapper={React.DOM.div} />
+			<img src={'/map_floor_2.png'} alt='Map of current floor of Regents Hall of Sciences' />
 			)
 	}
 }
