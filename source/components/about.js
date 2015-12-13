a7424634f0d13f27f9ce0c9fd633a73a2750691d
knightflight377 <emholm12@gmail.com>
2015-12-13 16:38:29 -0600

    Revised the about screen.

--- source/components/about.js
+++ source/components/about.js
@@ -13,8 +13,10 @@ class About extends React.Component {
 				<div>
 					<p className = {styles['about-text']}>
 					This web application was designed and built in the Computer Science Capstone course
-					in fall 2015. It was designed and built by Elinor Holm '16 and Kayla Bonnstetter '16. 
-					This application builds upon previous Capstone work, especially work done on the tile
+					in fall 2015. It was designed and built by Elinor Holm '16 and Kayla Bonnstetter '16. This
+					application is meant to be an early prototype for a Regents Navigation application. </p>
+
+					<p className = {styles['about-text']}> The application builds upon previous Capstone work, especially work done on the tile
 					server in fall 2014. The designers would also like to thank our classmates for their design 
 					input and Professor Hall-Holt for his guidance. The designers hope that the application is
 					fun and easy to use.
