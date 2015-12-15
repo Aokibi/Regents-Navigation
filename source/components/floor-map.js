ef1cac5f3ec65035e418afade5c0e46d5431cfce
Kayla Bonnstetter <bonnstet@stolaf.edu>
2015-12-15 16:16:41 -0600

    Started trying to make the map image displayed dependent on a select dropdown.

--- source/components/floor-map.js
+++ source/components/floor-map.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import {connect} from 'react-redux'
 
 /* Displays the 2D floor map. 
    Currently not implemented */
 class FloorMap extends React.Component {
+	static propTypes ={
+		floor: PropTypes.string.isRequired,
+	}
 	render() {
 		return(
-			<img src={'/images/map_floor_2.png'} alt='Map of current floor of Regents Hall of Sciences' />
+			<div>
+				<select value={this.props.optionsState}>
+					<option value='floor_1'>Floor 1</option>
+					<option value='floor_1-5'>Floor 1.5</option>
+					<option value='floor_2'>Floor 2</option>
+					<option value='floor_3'>Floor 3</option>
+					<option value='floor_4'>Floor 4</option>
+				</select>
+				<img src={'/images/map_{this.props.floor}.png'} alt='Map of current floor of Regents Hall of Sciences' />
+			</div>
 			)
 	}
 }
 
+function mapStateToProps(optionsState) {
+	return {
+		floor: optionsState,
+	}
+}
 
-
-export default FloorMap
+export default connect(mapStateToProps)(FloorMap)
