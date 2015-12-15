34b97a264883fc45b9247ad6b02a7633e1db234d
knightflight377 <emholm12@gmail.com>
2015-12-15 16:44:35 -0600

    Changed floor map to show all of the floors on the same page.

--- source/components/floor-map.js
+++ source/components/floor-map.js
@@ -1,32 +1,25 @@
-import React, { PropTypes } from 'react'
-import {connect} from 'react-redux'
+import React from 'react'
+import styles from './our-styles.less'
 
 /* Displays the 2D floor map. 
-   Currently not implemented */
+   Currently not fully implemented */
 class FloorMap extends React.Component {
-	static propTypes ={
-		floor: PropTypes.string.isRequired,
-	}
 	render() {
 		return(
 			<div>
-				<select value={this.props.optionsState}>
-					<option value='floor_1'>Floor 1</option>
-					<option value='floor_1-5'>Floor 1.5</option>
-					<option value='floor_2'>Floor 2</option>
-					<option value='floor_3'>Floor 3</option>
-					<option value='floor_4'>Floor 4</option>
-				</select>
-				<img src={'/images/map_{this.props.floor}.png'} alt='Map of current floor of Regents Hall of Sciences' />
+				<p className={styles['floor-text']}> Floor 1 </p>
+				<img src={'/images/map_floor_1.png'} alt='Map of current floor of Regents Hall of Sciences' /><br/>
+				<p className={styles['floor-text']}> Floor 1.5 </p>
+				<img src={'/images/map_floor_1-5.png'} alt='Map of current floor of Regents Hall of Sciences' /><br/>
+				<p className={styles['floor-text']}> Floor 2 </p>					
+				<img src={'/images/map_floor_2.png'} alt='Map of current floor of Regents Hall of Sciences' /><br/>
+				<p className={styles['floor-text']}> Floor 3 </p>
+				<img src={'/images/map_floor_3.png'} alt='Map of current floor of Regents Hall of Sciences' /><br/>
+				<p className={styles['floor-text']}> Floor 4 </p>
+				<img src={'/images/map_floor_4.png'} alt='Map of current floor of Regents Hall of Sciences' />								
 			</div>
 			)
 	}
 }
 
-function mapStateToProps(optionsState) {
-	return {
-		floor: optionsState,
-	}
-}
-
-export default connect(mapStateToProps)(FloorMap)
+export default FloorMap
